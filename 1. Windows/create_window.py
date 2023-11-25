e2ea28c82c50e545890856a56134be3af4a7fb04
Muhammad Hasnain Raza <138324430+HasnainRaza026@users.noreply.github.com>
2023-11-25 20:10:58 +0500

    Create create_window.py

---
+++ 1. Windows/create_window.py
@@ -0,0 +1,10 @@
+'''
+Creating a window
+'''
+
+# Import everything related to tkinter
+from tkinter import *
+
+window= Tk() #instantiate an instance of a window
+
+window.mainloop() #place window on computer screen, listen for events
