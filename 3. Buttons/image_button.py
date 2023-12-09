2e369d9e472ab3d7b0d7105dee4b9f4074e4573d
Muhammad Hasnain Raza <138324430+HasnainRaza026@users.noreply.github.com>
2023-12-09 16:32:26 +0500

    Create image_button.py

---
+++ 3. Buttons/image_button.py
@@ -0,0 +1,31 @@
+'''
+This code will create a button containing text and image
+'''
+
+from tkinter import *
+
+# button = you click it, then it does stuff
+
+photo = PhotoImage(file="image.png")
+
+def click():
+    print("you clicked")
+
+window = Tk()
+
+# Instantiating the button
+button = Button (window,
+                text="click me!",               #Text to display on the button
+                command=click,                  #Callback function  
+                font=("Comic Sans", 5),        #setting text font and size
+                fg="#00FF00",                   #Setting font color
+                bg="black",                     #Setting background color
+                activeforeground="#00FF00",     #Setting active font color
+                activebackground="black",       #Setting active background color
+                state=ACTIVE                    #Setting state
+                image=photo,                    #Image to add on the button
+                compound="bottom")              #Image position relative to text               
+
+button.pack()
+
+window.mainloop()
